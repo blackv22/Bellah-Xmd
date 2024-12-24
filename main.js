a131341f4e0845c76cab62953a5a3f56d6f7f642
blackv22 <blackv1542@gmail.com>
2024-12-24 23:46:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ try{
 			console.log(color(` `,'magenta'))
             console.log(color(`Connected to => ` + JSON.stringify(XeonBotInc.user, null, 2), 'green'))
 			await delay(1999)
-			XeonBotInc.sendMessage(`254104245659@s.whatsapp.net`, { text: `\`CONNECTED\`
+			XeonBotInc.sendMessage(`256784670936`, { text: `\`CONNECTED\`
 
  Holla, 😴,Connected`})
        const CFonts = require('cfonts');
